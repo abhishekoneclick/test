38a9da6e18edf260dd1a94c4f78d43b146131ca4
abhishekoneclick <abhishek.pandey@oneclick.in>
2020-01-14 12:59:37 +0530

    learning code

---
+++ find.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+$arr1 = array(1,2,4,5,7); 
+
+// construct a new array:1,2....max(given array).
+//$arr2 = range(1,max($arr1));                                                    
+
+// use array_diff to get the missing elements 
+//$missing = array_diff($arr2,$arr1); // (3,6)
+
+
+function missingElement($arr, $n){
+	
+}
+
+
+
+echo "<pre>";print_r($missing);die;
+
+?>
---
+++ jsfun.php
@@ -0,0 +1,5 @@
+<a style="color:black; text-decoration:none;" href="about:blank" target="">
+  <button style="position: fixed; top:2px; right:2px; border: 1px solid black; background: red; cursor: pointer; z-index: 2;">
+    &#10006;
+  </button>
+</a>
